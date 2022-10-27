0a006cc74b042ff38e96a3a26e8515ba3d40169e
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-10-27 12:49:53 +0300

    Create cu.java

---
+++ cu.java
@@ -0,0 +1,25 @@
+package com.company;
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "");
+        String a1 = JOptionPane.showInputDialog("fahrenheit hesaplaması için 1, celcius hesaplması için 2'yi tuşlayın");
+
+        int s1 = Integer.parseInt(a1);
+
+
+        if (s1 == 1) {
+            String a2 = JOptionPane.showInputDialog("celcius değeri gir");
+            int s2 = Integer.parseInt(a2);
+            double sum = 9 / 5 * (s2) + 32;
+            JOptionPane.showMessageDialog(null, sum + "Fahrenheit");
+        } else {
+            String a2 = JOptionPane.showInputDialog("Fahrenheit değeri gir");
+            int s2 = Integer.parseInt(a2);
+            double sum = (s2 - 32) * 5 / 9;
+            JOptionPane.showMessageDialog(null, sum + "celsius");
+        }
+
+    }
+    }
